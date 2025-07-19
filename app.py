2213142c3b410bfd2ca30d5d5a3835e2b0d71748
Isabele <104145203+isabele7@users.noreply.github.com>
2025-07-19 19:40:42 -0300

    importação de bibliotecas

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
 from dados import montar_dataframe
 from predicao import predizer_regressao, predizer_classificacao
 
